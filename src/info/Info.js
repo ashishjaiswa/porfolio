28d953641e26b1e299b4126db6b1afa53de60905
ashishjaiswa <ashishjaiswa1996@gmail.com>
2023-11-28 00:02:26 +0530

    build2

--- src/info/Info.js
+++ src/info/Info.js
@@ -103,7 +103,7 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
+            title: "SDP(Student Doubt panal)",
             live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
